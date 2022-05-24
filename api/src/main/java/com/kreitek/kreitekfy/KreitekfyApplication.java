074294fd6aaca00bc273fc3874636ab88a244d17
Alejandro Romero Pino <alejandro.romero.pino@gmail.com>
2022-05-24 14:12:13 +0200

    Commit Inicial

---
+++ api/src/main/java/com/kreitek/kreitekfy/KreitekfyApplication.java
@@ -0,0 +1,13 @@
+package com.kreitek.kreitekfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreitekfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreitekfyApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/kreitek/kreitekfy/KreitekfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreitek.kreitekfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreitekfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
