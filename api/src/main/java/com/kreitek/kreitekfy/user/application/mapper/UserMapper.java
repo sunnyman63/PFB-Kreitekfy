abddf8b23d6dc24903b7c1baa64f9b2abf4a583c
guipagra <guillermopaton94@gmail.com>
2022-05-25 12:38:45 +0200

    added new user mapper

---
+++ api/src/main/java/com/kreitek/kreitekfy/user/application/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.kreitek.kreitekfy.user.application.mapper;
+
+import com.kreitek.kreitekfy.shared.mapper.EntityMapper;
+import com.kreitek.kreitekfy.user.application.dto.UserDTO;
+import com.kreitek.kreitekfy.user.domain.entity.User;
+
+public interface UserMapper extends EntityMapper <UserDTO, User>{
+
+}
