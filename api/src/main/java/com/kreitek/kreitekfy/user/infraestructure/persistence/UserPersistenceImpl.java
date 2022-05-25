993d5a2434ab389cc5227b14def5ff8d5e0f6a13
guipagra <guillermopaton94@gmail.com>
2022-05-25 13:11:55 +0200

    Added user persistence in Insfraestructure & Domain

---
+++ api/src/main/java/com/kreitek/kreitekfy/user/infraestructure/persistence/UserPersistenceImpl.java
@@ -0,0 +1,48 @@
+package com.kreitek.kreitekfy.user.infraestructure.persistence;
+
+import com.kreitek.kreitekfy.album.domain.entity.Album;
+import com.kreitek.kreitekfy.user.application.mapper.UserMapper;
+import com.kreitek.kreitekfy.user.domain.entity.User;
+import com.kreitek.kreitekfy.user.domain.persistence.UserPersistence;
+import jdk.jfr.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserPersistenceImpl implements UserPersistence {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserPersistenceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUsersById(Long idUser) {
+        return this.userRepository.findById(idUser);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long idUser) {
+         this.userRepository.deleteById(idUser);
+    }
+
+    @Override
+    public List<Album> getUsersByName(String partialName) {
+        return null;
+    }
+}
