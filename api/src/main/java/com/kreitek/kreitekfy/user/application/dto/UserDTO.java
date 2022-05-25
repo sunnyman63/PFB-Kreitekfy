1278ab0d55c2b272a92468e2ccc8ff1ce6234917
guipagra <guillermopaton94@gmail.com>
2022-05-25 12:10:59 +0200

    added new User entity & User DTO

---
+++ api/src/main/java/com/kreitek/kreitekfy/user/application/dto/UserDTO.java
@@ -0,0 +1,35 @@
+package com.kreitek.kreitekfy.user.application.dto;
+
+public class UserDTO {
+
+    private Long idUser;
+    private boolean admin;
+    private String name;
+
+    public UserDTO() {
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUsuario) {
+        this.idUser = idUsuario;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
