d41f7e63b88146303536e7ca83e32b875641379d
JoseSSantos <josefsantos5004@gmail.com>
2022-05-25 13:54:32 +0200

    Added Artist and Style Service implementation

---
+++ api/src/main/java/com/kreitek/kreitekfy/style/application/service/impl/StyleServiceImpl.java
@@ -0,0 +1,55 @@
+package com.kreitek.kreitekfy.style.application.service.impl;
+
+import com.kreitek.kreitekfy.style.application.dto.StyleDTO;
+import com.kreitek.kreitekfy.style.application.mapper.StyleMapper;
+import com.kreitek.kreitekfy.style.application.service.StyleService;
+import com.kreitek.kreitekfy.style.domain.entity.Style;
+import com.kreitek.kreitekfy.style.domain.persistence.StylePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StyleServiceImpl implements StyleService {
+
+    private final StylePersistence stylePersistence;
+    private final StyleMapper styleMapper;
+
+    @Autowired
+    public StyleServiceImpl(StylePersistence stylePersistence, StyleMapper styleMapper) {
+        this.stylePersistence = stylePersistence;
+        this.styleMapper = styleMapper;
+    }
+
+    @Override
+    public List<StyleDTO> getAllStyles() {
+        List<Style> styles = this.stylePersistence.getAllStyles();
+        return styleMapper.toDto(styles);
+    }
+
+    @Override
+    public Optional<StyleDTO> getStyleById(Long styleId) {
+        return this.stylePersistence
+                .getStyleById(styleId)
+                .map(styleMapper::toDto);
+    }
+
+    @Override
+    public StyleDTO saveStyle(StyleDTO styleDTO) {
+        Style style = this.stylePersistence.saveStyle(this.styleMapper.toEntity(styleDTO));
+
+        return this.styleMapper.toDto(style);
+    }
+
+    @Override
+    public void deleteStyle(Long styleId) {
+        this.stylePersistence.deleteStyle(styleId);
+    }
+
+    @Override
+    public List<StyleDTO> getStyleByName(String partialName) {
+        return null;
+    }
+}
